1d54623ea57d4abfff26c8c73fcd3ae9ee07091a
Stian Lystad <Stianlystad_1@hotmail.com>
2022-03-29 11:46:10 +0000

    collect sales data

--- run.py
+++ run.py
@@ -105,6 +105,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    collects collums of data from sales worksheet
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+       column = sales.col_values(ind)
+       columns.append(column[-5:])
+    
+    return columns
+
+
 
 def main():
     """
@@ -118,4 +132,6 @@ def main():
 
 
 print("welcome to Love Sandwitches data automation")
-main()
+ # main()
+
+sales_columns = get_last_5_entries_sales()
